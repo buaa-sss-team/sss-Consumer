8b203f2673a9b3dffedd0edf871267b57cffff60
for0nething <245575413@qq.com>
2019-06-01 14:19:22 +0800

    添加对HDBdao新接口的测试

--- src/main/java/com/sss/consumer/TestHibernate.java
+++ src/main/java/com/sss/consumer/TestHibernate.java
@@ -15,7 +15,7 @@ public class TestHibernate {
     public static void main(String args[]){
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
         IHDBdao hdbdao=ctx.getBean(IHDBdao.class);
-        /*
+
         User x=new User();
         x.setId(1);
         x.setName("LiMing");
@@ -23,19 +23,27 @@ public class TestHibernate {
         x.setCredit(40);
         x.setPassword("Safe");
         x.setPersonalInformationJson("");
-        hdbdao.insertUser(x);
+        hdbdao.insert(x);
+       // hdbdao.insertUser(x);
 
         x.setId(3);
         x.setName("LiMing");
-        hdbdao.insertUser(x);
-        */
-        List<User> x;
-        x=hdbdao.getUserByName("LiMing");
+        hdbdao.insert(x);
+        //hdbdao.insertUser(x);
+
+
+        List<User> z;
+        z=hdbdao.getUserByName("LiMing");
         logger.info("#######################输出结果:#####################");
-        Iterator<User>it=x.iterator();
+        Iterator<User>it=z.iterator();
         while(it.hasNext()){
             Integer tem=it.next().getId();
             logger.info(tem.toString());
         }
+
+        User y=(User)hdbdao.getByID(User.class,1);
+        logger.info(y.getName());
+        logger.info(y.getPassword());
+
     }
 }
