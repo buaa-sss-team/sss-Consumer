a6c605eec11cdba3dcd267da51f811813ca7135c
zlccccc <940182901@qq.com>
2019-06-04 00:53:59 +0800

    test ES service

--- src/main/java/com/sss/consumer/TestESService.java
+++ src/main/java/com/sss/consumer/TestESService.java
@@ -2,10 +2,12 @@ package com.sss.consumer;
 
 import com.sss.interfaces.IESService;
 import com.sss.interfaces.dao.IHDBdao;
+import javafx.util.Pair;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -14,10 +16,20 @@ public class TestESService {
     public static void main(String []args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-consumer.xml");
         IESService hesservice=ctx.getBean(IESService.class);
-        List<Map<String, Object>> res = hesservice.FuzzyQueryAbstract("paper","abstract","abstra",0);
-        for (Map<String, Object> now:res) {
+
+        List<Pair<String, List<Pair<String, Pair<String, String>>>>> limits = new LinkedList<Pair<String, List<Pair<String, Pair<String, String>>>>>();
+        LinkedList<Pair<String, Pair<String, String>>> fir=new LinkedList<Pair<String, Pair<String, String>>>();
+        fir.add(new Pair("instruction",new Pair("matchQuery","sss")));
+        limits.add(new Pair("abstract",fir));
+        List<Map<String, Object>> res = hesservice.Query("paper",limits, 100);
+        if (res == null) {
+            System.out.println("数据获取失败");
+            return;
+        }
+        for (Map<String, Object> now : res) {
             System.out.println(now.toString());
         }
+
         System.out.println("END!!!!!!!!!!!!!!!!!");
     }
 }
