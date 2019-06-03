8522d8620e8cd1204c5dded3e38be5a047e732bb
for0nething <245575413@qq.com>
2019-06-03 10:43:47 +0800

    修改hibernate设置

--- src/main/java/com/sss/consumer/TestESService.java
+++ src/main/java/com/sss/consumer/TestESService.java
@@ -14,10 +14,10 @@ public class TestESService {
     public static void main(String []args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-consumer.xml");
         IESService hesservice=ctx.getBean(IESService.class);
-        List<Map<String, Object>> res = hesservice.FuzzyQueryAbstract("paper","abstract","test");
-        for (Map<String, Object> now:res) {
-            System.out.println(now.toString());
-        }
+        //List<Map<String, Object>> res = hesservice.FuzzyQueryAbstract("paper","abstract","test");
+        //for (Map<String, Object> now:res) {
+        //    System.out.println(now.toString());
+        //}
         System.out.println("END!!!!!!!!!!!!!!!!!");
     }
 }
